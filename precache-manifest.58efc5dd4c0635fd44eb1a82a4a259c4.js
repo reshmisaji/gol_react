7a40d627c319d21f11324fc3f8e14edfcc4dd506
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2019-04-03 11:53:45 +0530

    Updates

---
+++ precache-manifest.58efc5dd4c0635fd44eb1a82a4a259c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2613f348691bd558dc08",
+    "url": "/gol_react/static/css/main.4cb5372a.chunk.css"
+  },
+  {
+    "revision": "2613f348691bd558dc08",
+    "url": "/gol_react/static/js/main.e9475ecd.chunk.js"
+  },
+  {
+    "revision": "045d73e7b823ef06c90f",
+    "url": "/gol_react/static/js/runtime~main.1ef81186.js"
+  },
+  {
+    "revision": "9b3a5007987034dff77a",
+    "url": "/gol_react/static/js/2.652dea17.chunk.js"
+  },
+  {
+    "revision": "7a7f84c0990acd668121ecf44886a97c",
+    "url": "/gol_react/index.html"
+  }
+];
